4eaac8f5a3fe6add8d69c9e60a8441bafcb637cc
IT-Prince <>
2021-09-29 02:40:04 +0900

    Initial Commit

---
+++ src/utils/antd-icons.ts
@@ -0,0 +1,46 @@
+export {
+  // loading
+  LoadingOutline,
+  // close
+  CloseOutline,
+  // copy
+  CopyOutline,
+  // link
+  LinkOutline,
+  // notify
+  CheckCircleOutline,
+  // swap-alert
+  QuestionCircleOutline,
+  ExclamationCircleOutline,
+  CloseCircleOutline,
+  // wallet
+  WalletOutline,
+  // info&setting
+  InfoCircleOutline,
+  SettingOutline,
+  // down
+  CaretDownOutline,
+  // sort
+  ArrowUpOutline,
+  ArrowDownOutline,
+  // swap
+  SwapOutline,
+  // plus
+  PlusOutline,
+  // collapse
+  RightOutline,
+  // remove
+  MinusOutline,
+  // apy
+  CalculatorOutline,
+  // arrow
+  CaretDownFill,
+  CaretUpFill,
+  // search
+  SearchOutline,
+  // home-ui
+  DownOutline,
+  // pools
+  LeftOutline,
+  DeleteOutline
+} from '@ant-design/icons'
